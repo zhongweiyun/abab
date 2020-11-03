48dae03f449b2366e61e25d528bec37d30028277
zhongweiyun <1793874137@qq.com>
2020-11-03 20:14:39 +0800

    读写锁

---
+++ 10.03.go
@@ -0,0 +1,25 @@
+package main //P280 读写锁
+import(
+	"fmt"
+	"sync"
+	"time"
+)
+func main() {
+	var rwm sync.RWMutex
+	for i := 1; i <=3; i++ {
+		go func(i int) {
+			fmt.Printf("goroutine %d,尝试锁定。。\n", i)
+			rwm.RLock()
+			fmt.Printf("goroutine %d,已经读锁定了。。 \n", i)
+			time.Sleep(5 * time.Second)
+			fmt.Printf("goroutime %d,读解锁。。 \n", i)
+			rwm.RUnlock()
+		}(i)
+	}
+	time.Sleep(1*time.Second)
+	fmt.Println("main..尝试写锁定。。")
+	rwm.Lock()
+	fmt.Println("main..已经写锁定了。。")
+	rwm.Unlock()
+	fmt.Printf("main。。写解锁。。。")
+}
